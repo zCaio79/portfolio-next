b4b17e694745419bbd58a9e4909bd09ef007c5f5
zCaio79 <caiomendesfaria@gmail.com>
2025-07-30 00:20:49 -0300

    main section added, and navbar, working on some css effects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,61 @@
+import { CodeXml, Rat } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="flex flex-col w-full h-screen bg-zinc-900">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <header className="flex w-full items-center h-fit py-4 px-4 md:px-8 border-b-2 border-zinc-800 justify-between text-zinc-50">
+
+        <span>Caio Mendes | Portfólio</span>
+
+        <Rat className="size-5" />
+
+        <nav className="hidden md:flex text-zinc-200 gap-2 items-center">
+          <a href="" className="py-1.5 px-2 transition-all duration-150 hover:bg-zinc-800 rounded-sm">Sobre</a>
+          <a href="" className="py-1.5 px-2 transition-all duration-150 hover:bg-zinc-800 rounded-sm">Projetos</a>
+          <a href="" className="py-1.5 px-2 transition-all duration-150 hover:bg-zinc-800 rounded-sm">Contato</a>
+        </nav>
+
+      </header>
+
+      <section className="flex flex-col justify-center items-center mx-auto w-[65vw] md:w-[45vw] h-[80vh] gap-6 border-x-2 border-zinc-800">
+
+        <div className="flex flex-col-reverse gap-8 md:gap-14 justify-center items-center border-y-2 border-zinc-800 w-[100vw] md:w-[100vw] p-8 transition-all duration-500 md:flex-row-reverse 
+        hover:w-[70vw] hover:rounded-md">
+          <span className="flex self-center justify-center md:self-start h-fit md:h-[24vh] flex-col font-bold text-6xl text-zinc-50 transition-all duration-500 border-y-2 border-zinc-800 px-6 md:text-7xl">
+            <span className="md:justify-start justify-center border-x-2 border-zinc-800 px-4 py-1.5 flex w-[80vw] md:w-full md:h-[20vh] transition-all duration-500 hover:text-8xl">CAIO</span>
+            <span className="md:justify-start justify-center border-x-2 border-zinc-800 px-4 py-1.5 flex w-[80vw] md:w-full md:h-[20vh] transition-all duration-500 hover:text-6xl">MENDES</span>
+          </span>
+
+          <Image
+            className=" rounded-bl-full rounded-tr-full rounded-tl-full border-4 border-zinc-100 transition-all duration-500 hover:rounded-t-4xl hover:rounded-br-full"
+            src="/caio.jpeg"
+            width={180} height={180}
+            alt="caio"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="flex items-center flex-col gap-4 px-4 border-y-2 border-zinc-800 w-[55vw] p-6 transition-all duration-500 
+        hover:w-[100vw] hover:rounded-md">
+          <span className="flex md:w-[20vw] justify-center flex-col md:flex-row items-start md:items-end border-2 border-zinc-800 mt- py-1.5 gap-2 px-6 md:gap-4 text-zinc-400 text-lg
+          transition-all duration-500 hover:w-[35vw]">
+            Desenvolvedor Frontend
+            <CodeXml className="size-6" />
+          </span>
+
+          <span className="hidden md:flex text-center w-fit justify-center gap-1.5 font-light py-2 px-2 md:px-4 rounded-sm bg-zinc-700 text-zinc-50 transition duration-500 hover:w-[35vw]">
+            Tornando a sua ideia em uma <span className="font-medium">experiência visual</span>
+          </span>
+
+        </div>
+
+      </section>
+
+      <section className="flex w-full border-t-2 border-zinc-800">
+
+      </section>
+
     </div>
-  );
+  )
 }
