5a178c8f4d84445f6ccec5e2ece7623cc1b3fc16
zCaio79 <caiomendesfaria@gmail.com>
2025-08-11 18:35:56 -0300

    about img fix

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -9,7 +9,7 @@ export default function About(props : AboutProps){
     return(
         <section id={props.id} className="flex flex-col justify-center w-full h-[65vh] md:h-[80vh] md:justify-center items-center gap-6 py-6 px-6">
 
-        <div className="flex flex-col justify-center w-[70vw] md:w-[55vw] gap-4 h-fit md:h-[54vh] md:min-h-fit border-2 border-zinc-700 py-6 md:px-12 transition-all duration-500 hover:w-[60vw] hover:shadow-2xl hover:rounded">
+        <div className="flex flex-col justify-center w-[90vw] md:w-[55vw] gap-4 h-fit md:h-[54vh] md:min-h-fit border-2 border-zinc-700 py-6 md:px-12 transition-all duration-500 hover:w-[60vw] hover:shadow-2xl hover:rounded">
 
           <span className="flex w-fit gap-6 items-center text-zinc-50 font-semibold text-5xl border-b-2 border-zinc-700 pb-3 px-6 md:pr-3 md:pl-0 transition-all duration-500 hover:text-6xl">Sobre Mim <Rabbit className="hidden md:flex size-10"/></span>
           <span className="flex flex-col md:flex-row justify-between items-center gap-6 py-6 px-2 text-sm md:text-base text-zinc-300 border-y-2 border-zinc-700">
@@ -17,7 +17,7 @@ export default function About(props : AboutProps){
               Sou desenvolvedor front-end e trabalho transformando ideias em interfaces modernas, funcionais e responsivas. Gosto de unir design e código para criar experiências visuais que fazem sentido tanto para quem usa quanto para quem idealiza. Acredito que um bom site vai além da aparência, ele precisa ser leve, intuitivo e acessível em qualquer dispositivo.
             </span>
             <Image
-              className="hidden saturate-0 md:flex border-4 md:ml-auto border-zinc-100 transition-all duration-500 hover:saturate-[80%]"
+              className=" saturate-0 flex border-4 md:ml-auto border-zinc-100 transition-all duration-500 hover:saturate-[80%]"
               src="/caio01.jpg"
               width={200} height={200}
               alt="caio"
